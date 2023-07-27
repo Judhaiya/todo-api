f017c420602ebab1a43e24693544f06fed3b15ea
jeyaudhaiya <udiAaral11@gmail.com>
2023-07-27 08:00:30 +0530

    testing token functions

--- tests/services/tokenGeneration.test.js
+++ tests/services/tokenGeneration.test.js
@@ -1,12 +1,12 @@
-const {verifyToken,generateToken} = require("../../services/token");
+const { verifyToken, generateToken } = require("../../services/token");
 const chai = require("chai");
 
 const expect = chai.expect;
 
 const userEmail = "levia14@gmail.com";
-describe("ttesting verification and token generation", () => {
-  it("verify token", () => {
-     generateToken(userEmail);
-     console.log(generateToken(userEmail));
-  })
-})
+describe("verification and token generation function works perfectly", () => {
+  it("if on token verfication it generates the expected email", () => {
+    const tokenVerifiedEmail = verifyToken(generateToken(userEmail)).payload;
+    expect(tokenVerifiedEmail).to.equal(userEmail);
+  });
+});
